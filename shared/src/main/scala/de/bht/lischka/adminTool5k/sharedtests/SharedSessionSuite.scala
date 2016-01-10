b2c34391ffd483cad9c71e1830bebfc0185fc86e
Simon Lischka <simon@a-studios.org>
2016-01-10 19:10:50 +0100

    Pass tests with manual message filter (Introduction of UnpickledMessageFromNetwork)

--- shared/src/main/scala/de/bht/lischka/adminTool5k/sharedtests/SharedSessionSuite.scala
+++ shared/src/main/scala/de/bht/lischka/adminTool5k/sharedtests/SharedSessionSuite.scala
@@ -1,7 +1,8 @@
 package de.bht.lischka.adminTool5k.sharedtests
 
-import akka.actor.ActorSystem
-import de.bht.lischka.adminTool5k.InternalMessages.{RegisterListener, PickledMessageForSending}
+import akka.actor.{ActorRef, Props, Actor, ActorSystem}
+import akka.testkit.TestProbe
+import de.bht.lischka.adminTool5k.InternalMessages.{UnpickledMessageFromNetwork, RegisterListener, PickledMessageForSending}
 import de.bht.lischka.adminTool5k.ModelX.{LoginUser, TestWSMessage, User}
 import de.bht.lischka.adminTool5k.Session
 import utest._
@@ -17,26 +18,41 @@ import scala.concurrent.duration._
 
 object SharedSessionSuite {
   def sessionTests()(implicit testProbe: () => AbstractTestProbe) = TestSuite {
+    object ManualFilter {
+      def props(target: ActorRef) = Props(new ManualFilter(target))
+    }
+
+    class ManualFilter(target: ActorRef) extends Actor {
+      def receive = {
+        case RegisterListener(x) =>
+        case otherMsg =>
+          target ! otherMsg
+      }
+    }
+
     'sessionTests {
       implicit val system = ActorSystem()
-      val websocketOut = testProbe()
-      val router = testProbe()
-      val probe = testProbe()
-      val session = system.actorOf(Session.props(websocketOut.ref, router.ref), "Session")
+      val websocketOut = TestProbe()
+      val router = TestProbe()
+      /**
+        * Does not seem to ignore msg properly
+        */
+      router.ignoreMsg { case RegisterListener => true }
+      val probe = TestProbe()
+      val manualFilterToRouter = system.actorOf(ManualFilter.props(router.ref))
+      val session = system.actorOf(Session.props(websocketOut.ref, manualFilterToRouter), "Session")
+
 
       'loggedInUserForwardsMessageToRouter {
         val testMessage = TestWSMessage("loggedInUserForwardsMessageToRouter")
         probe.send(session, LoginUser(User("TestUser")))
-        probe.send(session, testMessage)
-//        router.ignoreMessage { case RegisterListener => true }
-        router.expectMsg(500 millis, RegisterListener)
+        probe.send(session, UnpickledMessageFromNetwork(testMessage))
         router.expectMsg(500 millis, testMessage)
       }
 
       'loggedOutUserDoesNotForwardMessageToRouter {
         val testMessage = TestWSMessage("loggedOutUserDoesNotForwardMessageToRouter")
-        probe.send(session, testMessage)
-        router.expectMsg(500 millis, RegisterListener)
+        probe.send(session, UnpickledMessageFromNetwork(testMessage))
         router.expectNoMsg(500 millis)
       }
 
