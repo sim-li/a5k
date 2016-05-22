491f33aeb16673fd1b26532900b7e14a99642c4c
Simon Lischka <simon@a-studios.org>
2016-05-22 16:52:54 +0200

    Change style of picklesupport

--- shared/src/main/scala/de/bht/lischka/adminTool5k/pickling/PickleSupport.scala
+++ shared/src/main/scala/de/bht/lischka/adminTool5k/pickling/PickleSupport.scala
@@ -8,13 +8,12 @@ import de.bht.lischka.adminTool5k.ModelX.WSMessage
   * Naming actor: Actor => with self: Actor overrides sender reference:
   * http://coding-journal.com/why-is-my-akka-actor-getting-deadletters-as-sender/
   */
-trait PickleSupport {
-  actor: Actor =>
-  val pickling = context.actorOf(Pickling.props)
-  val unpickling = context.actorOf(Unpickling.props())
+trait PickleSupport extends Actor {
+    val pickling = context.actorOf(Pickling.props)
+    val unpickling = context.actorOf(Unpickling.props())
 
-  def handlePickling: Receive = {
-    case serializedString: String => unpickling ! serializedString
-    case SendMessage(msg: WSMessage) => pickling ! msg
-  }
+    def handlePickling: Receive = {
+      case serializedString: String => unpickling ! serializedString
+      case SendMessage(msg: WSMessage) => pickling ! msg
+    }
 }
